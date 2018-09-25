0d6cd0dda7ea4fa986279bbba758a737f8be6807
abhi <abigambhir@gmail.com>
2018-09-25 23:32:15 +0530

    added static quote implementation

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <app-quote-grid :quotes="quotesArray"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import quoteGrid from './components/quoteGrid.vue';
+
+    export default {
+        data: function(){
+            return{
+                quotesArray: [
+                    'Legacy is greater than currency.',
+                    'Without hustle, your talent will only get you so far.'
+                    ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: quoteGrid
+        }
+    }
+</script>
+
+<style>
+</style>
