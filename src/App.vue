a22758fd05c2ac78a19406ed8d83e2183973be64
abhi <abigambhir@gmail.com>
2018-09-26 00:54:05 +0530

    added on click quote delete functionality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="container">
         <app-new-quote @quoteAddEmit="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotesArray"></app-quote-grid>
+        <app-quote-grid :quotes="quotesArray" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on Quote to delete it!</div> 
+            </div> 
+        </div>      
     </div>
 </template>
 
@@ -22,6 +27,9 @@
         methods:{
             newQuote(quote){
                 this.quotesArray.push(quote);
+            },
+            deleteQuote(index){
+                this.quotesArray.splice(index,1);
             }
         },
         components: {
