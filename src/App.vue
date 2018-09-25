ec92b0447b625db172aa5cf6b06cb58ed32ae35b
abhi <abigambhir@gmail.com>
2018-09-26 00:35:26 +0530

    added functionality to add quotes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container">
+        <app-new-quote @quoteAddEmit="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotesArray"></app-quote-grid>
     </div>
 </template>
 
 <script>
     import quoteGrid from './components/quoteGrid.vue';
+    import newQuote from './components/newQuote.vue';
 
     export default {
         data: function(){
@@ -17,8 +19,14 @@
                 maxQuotes: 10
             }
         },
+        methods:{
+            newQuote(quote){
+                this.quotesArray.push(quote);
+            }
+        },
         components: {
-            appQuoteGrid: quoteGrid
+            appQuoteGrid: quoteGrid,
+            appNewQuote: newQuote
         }
     }
 </script>
---
+++ src/components/newQuote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>        
+            </div>
+            <div class="col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNewQuote">Add Quote</button>  
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+       data: function() {
+           return{
+               quote: ''
+           }
+       },
+       methods:{
+           createNewQuote(){
+               this.$emit('quoteAddEmit',this.quote);
+               this.quote = '';
+           }
+       }
+    }
+</script>
+
+<style>
+</style>
